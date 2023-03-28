c764c2581c3583fca663238716c5a93b56ac17f3
Farias123 <vitorcostafarias@gmail.com>
2023-03-28 00:35:46 -0300

    Update criar-tabelas.sql
    Melhorei a diagramação

--- criar-tabelas.sql
+++ criar-tabelas.sql
@@ -4,41 +4,37 @@ CREATE DATABASE IF NOT EXISTS empresa_generica;
 
 
 CREATE TABLE IF NOT EXISTS produtos(
-nome TEXT,
-descricao TEXT,
-preco NUMERIC(10,2),
-categoria TEXT
+    nome TEXT,
+    descricao TEXT,
+    preco NUMERIC(10,2),
+    categoria TEXT
 );
 
 
 CREATE TABLE IF NOT EXISTS pedidos(
-pedido TEXT,
-data DATE,
-endereco TEXT,
-preco_total NUMERIC(10,2),
-nome_cliente TEXT
+    pedido TEXT,
+    descricao TEXT,
+    data DATE,
+    endereco TEXT,
+    preco_total NUMERIC(10,2),
+    nome_cliente TEXT,
+    quantidade_vendida INTEGER
 );
 
 
 CREATE TABLE IF NOT EXISTS pedidos_vendidos AS
-SELECT DISTINCT ON (pedido) * 
-FROM pedidos;
+    SELECT DISTINCT ON (pedido) *
+    FROM pedidos;
 
 
 CREATE TABLE IF NOT EXISTS categorias AS 
-SELECT categoria, COUNT(*) AS num_produtos 
-FROM Produtos 
-GROUP BY categoria;
+    SELECT categoria, COUNT(*) AS num_produtos 
+    FROM Produtos 
+    GROUP BY categoria;
 
 
 CREATE TABLE IF NOT EXISTS produtos_replicados AS 
-SELECT nome, COUNT(*) - 1 AS quantidade_replicacoes 
-FROM Produtos 
-GROUP BY nome 
-HAVING COUNT(*) > 1;
-
-DROP TABLE produtos;
-DROP TABLE pedidos;
-DROP TABLE pedidos_vendidos;
-DROP TABLE categorias;
-DROP TABLE produtos_replicados;
+    SELECT nome, COUNT(*) - 1 AS quantidade_replicacoes 
+    FROM Produtos 
+    GROUP BY nome 
+    HAVING COUNT(*) > 1;
