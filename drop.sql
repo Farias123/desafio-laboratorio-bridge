81f5cae8afe0c3aa74622b32890f4f4b04fd1329
Farias123 <vitorcostafarias@gmail.com>
2023-03-28 00:35:58 -0300

    Create drop.sql

---
+++ drop.sql
@@ -0,0 +1,8 @@
+DROP FUNCTION criar_produtos;
+DROP FUNCTION criar_pedidos;
+
+DROP TABLE produtos;
+DROP TABLE pedidos;
+DROP TABLE pedidos_vendidos;
+DROP TABLE categorias;
+DROP TABLE produtos_replicados;
