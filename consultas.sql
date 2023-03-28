73f2afa7c040a24366053bf654df6f472fddf601
Farias123 <vitorcostafarias@gmail.com>
2023-03-28 00:37:18 -0300

    Update consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -1,18 +1,18 @@
 SELECT nome, descricao, preco FROM produtos
 ORDER BY nome ASC;
 
-SELECT * FROM Categorias
+SELECT categoria, num_produtos FROM Categorias
 ORDER BY categoria ASC;
 
 SELECT data, endereco, preco_total FROM pedidos
 ORDER BY data DESC;
 
-SELECT * FROM pedidos_vendidos;
+SELECT pedido, descricao, preco_total, quantidade_vendida FROM pedidos_vendidos
 ORDER BY quantidade_vendida DESC;
 
 SELECT pedido FROM pedidos
-WHERE nome_cliente = 'Cliente1' AND data <= '2023-04-01' AND data >= '2023-03-01'
+WHERE nome_cliente = 'Cliente3' AND data <= '2023-04-01' AND data >= '2023-03-01'
 ORDER BY nome_cliente ASC, data ASC;
 
-SELECT * FROM produtos_replicados
+SELECT nome, quantidade_replicacoes FROM produtos_replicados
 ORDER BY quantidade_replicacoes DESC;
